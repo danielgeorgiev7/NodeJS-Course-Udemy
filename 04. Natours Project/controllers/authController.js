023f280a1b8a02341501ca3b533f015572652722
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-17 14:56:35 +0200

    Added protection to get request

--- 04. Natours Project/controllers/authController.js
+++ 04. Natours Project/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -44,4 +45,35 @@ exports.login = catchAsync(async (req, res, next) => {
         status: 'success',
         token
     })
-})
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    };
+
+    if (!token) {
+        return next(
+            new AppError('You are not logged in! Please log in to get access.', 401)
+        );
+    };
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError('The user belonging to this token does no longer exist.', 401)
+        );
+    }
+
+    if (currentUser.changesPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again.', 401)
+        );
+    }
+
+    req.user = currentUser;
+    next();
+});
