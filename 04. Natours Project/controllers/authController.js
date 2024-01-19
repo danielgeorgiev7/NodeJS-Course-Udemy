54b35f0e5567eb310e2a43dc301d670b58b7efa4
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-01-19 15:03:07 +0200

    Added cookie and security middleware

--- 04. Natours Project/controllers/authController.js
+++ 04. Natours Project/controllers/authController.js
@@ -8,6 +8,16 @@ const crypto = require('crypto');
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRATION * 25 * 60 * 60 * 1000),
+        httpOnly: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+    res.cookie('jwt', token, cookieOptions);
+
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
