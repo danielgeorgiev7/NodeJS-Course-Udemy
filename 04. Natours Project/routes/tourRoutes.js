d86287cd9043165fc0843f350311472fac0e7b2c
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-11 18:24:02 +0200

    Add reviews router

--- 04. Natours Project/routes/tourRoutes.js
+++ 04. Natours Project/routes/tourRoutes.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-router
-    .route('/top-5-cheap')
-    .get(tourController.aliasTopTours, tourController.getAllTours);
+router.use('/:tourId/reviews', reviewRouter);
+
+router.route('/top-5-cheap')
+    .get(
+        tourController.aliasTopTours,
+        tourController.getAllTours
+    );
 
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router
-    .route('/')
+router.route('/')
     .get(authController.protect, tourController.getAllTours)
     .post(tourController.createTour);
 
-router
-    .route('/:id')
+router.route('/:id')
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(authController.protect, authController.restrictTo('admin', 'lead-guide'), tourController.deleteTour);
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.deleteTour
+    );
+
 
 module.exports = router;
