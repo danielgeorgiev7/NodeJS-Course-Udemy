de81e23164b00e92c966d335c3082784c5f383d6
nymuelis <daniel.tsv.georgiev@gmail.com>
2024-02-11 18:23:44 +0200

    Add locations and guides to tour model

--- 04. Natours Project/models/tourModel.js
+++ 04. Natours Project/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -76,7 +77,37 @@ const tourSchema = new mongoose.Schema({
     secretTour: {
         type: Boolean,
         default: false,
-    }
+    },
+    startLocation: {
+        //GeoJSON
+        type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+    },
+    locations: [
+        {
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point']
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+            day: Number
+        }
+    ],
+    guides: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+        }
+    ]
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -86,11 +117,25 @@ tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+});
+
 // before .save() and .create()
 tourSchema.pre('save', function (next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 });
+
+// tourSchema.pre('save', async function (next) {
+//     const guidesPromises = this.guides.map(async id => await User.findById(id));
+
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 // tourSchema.pre('save', function(next){
 //     console.log('Will save document...');
@@ -101,6 +146,14 @@ tourSchema.pre('save', function (next) {
 //     console.log(doc);
 //     next();
 // });
+
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    });
+    next();
+});
 
 tourSchema.pre(/^find/, function (next) {
     this.find({ secretTour: { $ne: true } });
